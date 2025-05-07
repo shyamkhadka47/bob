3fc5f182f3703f5d435e5e1df81aa39961f083ff
Shyam Khadka <letuscode47@gmail.com>
2025-05-07 14:58:16 +0545

    adding font

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="bg-black text-white mt-16">
+    <footer className="bg-black text-white mt-16 font-serif">
       <div className="px-[5%] md:px-[10%] py-10">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8">
           <div className=''>
