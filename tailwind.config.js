4cc1dad43b3af8cc6bcace08b8e81142bc910606
Shyam Khadka <letuscode47@gmail.com>
2025-05-07 16:38:58 +0545

    testing

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,10 @@ module.exports = {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+      // colors: {
+      //   background: "var(--background)",
+      //   foreground: "var(--foreground)",
+      // },
     },
   },
   plugins: [],
